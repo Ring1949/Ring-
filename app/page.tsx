42470090ff70425f873615ccd8df09e51d9f3e28
Ring1949 <2836850342@qq.com>
2026-08-12 17:19:10 +0800

    fix: isolate spotlight portfolio layout

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700;800&family=Noto+Sans+SC:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
-      <link rel="stylesheet" href="/styles.css?v=20260812-spotlight-library-1" />
+      <link rel="stylesheet" href="/styles.css?v=20260812-spotlight-library-2" />
       <div dangerouslySetInnerHTML={{ __html: bodyHtml }} />
       <HomeInteractiveOverlays />
       <Script src="/common.js?v=20260812-nav-sync-1" strategy="afterInteractive" />
